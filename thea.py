081b281f46ce4d2e0a359e38632fc67532f40619
theajaffe <theajaffe@gmail.com>
2022-08-19 15:30:14 -0400

    init commit

--- thea.py
+++ thea.py
@@ -1,31 +1,20 @@
 import requests
 
-def recipe_search(ingredient):
-
-  edamam_app_key = 'b4bd766ee7762ee33c6758532257181c'
-
-  edamam_app_id = '753f0d79'
-
-  user_ingredient1 = input("what is your first ingredient?")
-
-  results = requests.get(
-    'https://api.edamam.com/search?q={}&app_id={}&app_key={}'.format(user_ingredient1, edamam_app_id, edamam_app_key))
 
 
-  data = results.json()
+edamam_app_key = 'b4bd766ee7762ee33c6758532257181c'
+
+edamam_app_id = '753f0d79'
+
+user_ingredient1 = input("what is your first ingredient?")
+
+results = requests.get(
+  'https://api.edamam.com/search?q={}&app_id={}&app_key={}'.format(user_ingredient1, edamam_app_id, edamam_app_key))
+
+
+data = results.json()
   
-  print(data)
-
-  return data[hits]
-
-  def run():
-    ingredient = user_ingredient1
-    results = data(ingredient)
-
-    for result in results:
-      recipe = result['recipe']
-      print(recipe['label'])
-      print(recipe['uri'])
+print(data)
 
 
 
