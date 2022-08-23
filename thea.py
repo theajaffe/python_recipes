8f5c3e6ae467de980232b340eb0339a257c4271f
theajaffe <theajaffe@gmail.com>
2022-08-23 15:43:04 -0400

    init commit

--- thea.py
+++ thea.py
@@ -5,25 +5,64 @@ edamam_app_key = 'b4bd766ee7762ee33c6758532257181c'
 
 edamam_app_id = '753f0d79'
 
+def one_dietary_restriction():
+  results = requests.get(
+    'https://api.edamam.com/search?q={}&app_id={}&app_key={}&health={}'.format(user_ingredient1, edamam_app_id, edamam_app_key, user_health_concern))
+
+  data = results.json()
+  data_keys = data.keys()
+
+  print( 'https://api.edamam.com/search?q={}&app_id={}&app_key={}'.format(user_ingredient1, edamam_app_id, edamam_app_key))
+
+  print(data_keys)
+
+  print('There are {} {} recipes that use {}.'.format((data['count']), user_health_concern, user_ingredient1))
+
+  print('See below for the top 10 results:')
+
+  recipe_hits = (data['hits'])
+
+  for each_recipe in recipe_hits:
+    recipe = each_recipe['recipe']
+    print(recipe['label'])
+    print(recipe['uri'])
+
+def two_dietary_restrictions():
+    results = requests.get(
+      'https://api.edamam.com/search?q={}&app_id={}&app_key={}&health={}&health={}'.format(user_ingredient1, edamam_app_id,
+                                                                                 edamam_app_key, user_health_concern, user_health_concern2))
+
+    data = results.json()
+    data_keys = data.keys()
+
+    print(
+      'https://api.edamam.com/search?q={}&app_id={}&app_key={}'.format(user_ingredient1, edamam_app_id, edamam_app_key))
+
+    print(data_keys)
+
+    print('There are {} {}, {} recipes that use {}.'.format((data['count']), user_health_concern, user_health_concern2, user_ingredient1))
+
+    print('See below for the top 10 results:')
+
+    recipe_hits = (data['hits'])
+
+    for each_recipe in recipe_hits:
+      recipe = each_recipe['recipe']
+      print(recipe['label'])
+      print(recipe['uri'])
+
+
 user_ingredient1 = input("what is your first ingredient?")
+user_health_concern = input('what is your dietary restriction?')
 
-results = requests.get(
-  'https://api.edamam.com/search?q={}&app_id={}&app_key={}'.format(user_ingredient1, edamam_app_id, edamam_app_key))
+further_restrictions = input('Do you have further dietary restrictions?')
 
-data = results.json()
-data_keys = data.keys()
+if further_restrictions == 'no':
+  one_dietary_restriction()
 
-print(data_keys)
-
-print('There are {} recipes that use {}.'.format((data['count']), user_ingredient1))
-
-print('See below for further detail:')
-
-recipe_hits = (data['hits'])
-
-for each_recipe in recipe_hits:
-  print(each_recipe)
-
+elif further_restrictions == 'yes':
+  user_health_concern2 = input('what other restriction do you have?')
+  two_dietary_restrictions()
 
 
 
