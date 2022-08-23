03b4efa04ecd2a227419042980c5e51dc01bded9
theajaffe <theajaffe@gmail.com>
2022-08-23 14:03:45 -0400

    init commit

--- thea.py
+++ thea.py
@@ -1,6 +1,5 @@
 import requests
-
-
+from pprint import pprint
 
 edamam_app_key = 'b4bd766ee7762ee33c6758532257181c'
 
@@ -11,14 +10,19 @@ user_ingredient1 = input("what is your first ingredient?")
 results = requests.get(
   'https://api.edamam.com/search?q={}&app_id={}&app_key={}'.format(user_ingredient1, edamam_app_id, edamam_app_key))
 
-
 data = results.json()
-  
-print(data)
+data_keys = data.keys()
 
+print(data_keys)
 
+print('There are {} recipes that use {}.'.format((data['count']), user_ingredient1))
 
+print('See below for further detail:')
 
+recipe_hits = (data['hits'])
+
+for each_recipe in recipe_hits:
+  print(each_recipe)
 
 
 
