a4bc80cee369bf1d16f7e62dac82c72acb9ec730
theajaffe <107112870+theajaffe@users.noreply.github.com>
2022-08-29 12:17:55 -0400

    init commit

--- thea.py
+++ thea.py
@@ -13,8 +13,6 @@ def one_dietary_restriction():
 
   data = results.json()
 
-  print( 'https://api.edamam.com/search?q={}&app_id={}&app_key={}'.format(user_ingredient1, edamam_app_id, edamam_app_key))
-
   print('We found {} {} recipes without {} that use {}.'.format((data['count']), user_health_concern, user_aversion, user_ingredient1))
 
   print('See below for the top 10 results:')
@@ -25,20 +23,20 @@ def one_dietary_restriction():
     recipe = each_recipe['recipe']
     print(recipe['label'])
     print(recipe['shareAs'])
+  print('For more info, please see:')
+  print( 'https://api.edamam.com/search?q={}&app_id={}&app_key={}'.format(user_ingredient1, edamam_app_id, edamam_app_key))
 
 
 def two_dietary_restrictions():
     results = requests.get(
       'https://api.edamam.com/search?q={}&app_id={}&app_key={}&health={}&health={}&excluded={}&dishType={}'.format(user_ingredient1, edamam_app_id,
-                                                                                 edamam_app_key, user_health_concern, user_health_concern2, user_aversion, dish_type))
+                                                                                 edamam_app_key, user_health_concern, further_restrictions, user_aversion, dish_type))
 
     data = results.json()
 
-    print(
-      'https://api.edamam.com/search?q={}&app_id={}&app_key={}'.format(user_ingredient1, edamam_app_id, edamam_app_key))
-
-
-    print('We found {} {}, {} recipes without {} that use {}.'.format((data['count']), user_health_concern, user_health_concern2, user_aversion, user_ingredient1, dish_type))
+    print('We found {} {}, {} recipes without {} that use {}.'.format((data['count']), user_health_concern,
+                                                                      further_restrictions, user_aversion,
+                                                                      user_ingredient1, dish_type))
 
     print('See below for the top 10 results:')
 
@@ -49,20 +47,29 @@ def two_dietary_restrictions():
       print(recipe['label'])
       print(recipe['shareAs'])
 
+    print('For more info, see:')
+    print(
+        'https://api.edamam.com/search?q={}&app_id={}&app_key={}'.format(user_ingredient1, edamam_app_id,
+                                                                         edamam_app_key))
+
 
 user_ingredient1 = input("what is your first ingredient?")
 user_aversion = input("which ingredient are you avoiding?")
 user_health_concern = input('what is your dietary restriction?')
 
-further_restrictions = input('Do you have further dietary restrictions?')
+further_restrictions = input('Do you have a further dietary restriction? If so, what is it?')
 
 if further_restrictions == 'no':
-  one_dietary_restriction()
+    try:
+        one_dietary_restriction()
+    except TypeError:
+        print('{} is not a recognised dietary restriction in Edamame.  Check spelling and try running the search again.'.format(user_health_concern))
 
-
-elif further_restrictions == 'yes':
-  user_health_concern2 = input('what other restriction do you have?')
-  two_dietary_restrictions()
+else:
+    try:
+        two_dietary_restrictions()
+    except TypeError:
+        print('Either {} or {} is not a recognised dietary restriction in Edamame.  Check spelling and try running the search again.'.format(user_health_concern, further_restrictions))
 
 
 
